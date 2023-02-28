7c1874af36ad48c7a2c75c2892668494513e3d21
Kyo Takata <kyotakata1@gmail.com>
2023-03-01 08:12:18 +0900

    22.#17-2_最初のテスト

---
+++ DDDTest.Tests/WeatherLatestViewModelTest.cs
@@ -0,0 +1,21 @@
+﻿using DDD.WinForm.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DDDTest.Tests
+{
+    [TestClass]
+    public class WeatherLatestViewModelTest
+    {
+        [TestMethod]
+        public void シナリオ()
+        {
+            var viewModel = new WeatherLatestViewModel();
+            Assert.AreEqual("", viewModel.AreaIdText);
+            Assert.AreEqual("", viewModel.DataDateText);
+            Assert.AreEqual("", viewModel.ConditionText);
+            Assert.AreEqual("", viewModel.TemperatureText);
+
+        }
+    }
+}
