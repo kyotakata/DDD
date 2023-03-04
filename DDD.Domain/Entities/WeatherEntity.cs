22bacadc0c2b2d4ad95b2fa50a2868416b9914e5
Kyo Takata <kyotakata1@gmail.com>
2023-03-04 10:20:47 +0900

    25.#20_Entityの実装

---
+++ DDD.Domain/Entities/WeatherEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Domain.Entities
+{
+    public class WeatherEntity
+    {
+        public WeatherEntity(int areaId, 
+            DateTime dataDate, 
+            int condition,
+            float temperature)
+        {
+            AreaId= areaId;
+            DataDate = dataDate;
+            Condition= condition;
+            Temperature= temperature;
+        }
+
+        public int AreaId { get; }
+        public DateTime DataDate { get; }
+        public int Condition { get; }
+        public float Temperature { get; }
+    }
+}
--- DDD.WinForm/ViewModels/WeatherLatestViewModel.cs
+++ DDD.WinForm/ViewModels/WeatherLatestViewModel.cs
@@ -24,12 +24,12 @@ namespace DDD.WinForm.ViewModels
 
         public void Search()
         {
-            var dt = _ｗeather.GetLatest(Convert.ToInt32(AreaIdText));
-            if (dt.Rows.Count > 0)
+            var entity = _ｗeather.GetLatest(Convert.ToInt32(AreaIdText));
+            if (entity != null)
             {
-                DataDateText = dt.Rows[0]["DataDate"].ToString();
-                ConditionText = dt.Rows[0]["Condition"].ToString();
-                TemperatureText = CommonFunc.RoundString(Convert.ToSingle(dt.Rows[0]["Temperature"]),
+                DataDateText = entity.DataDate.ToString();
+                ConditionText = entity.Condition.ToString();
+                TemperatureText = CommonFunc.RoundString(entity.Temperature,
                     CommonConst.TemperatureDecimalPoint) + " "
                     + CommonConst.TemperatureUnitName;
             }
