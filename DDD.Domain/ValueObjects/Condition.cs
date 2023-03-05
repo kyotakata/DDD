ec11a9be32c71176d4a101980be4d31670acd8e3
Kyo Takata <kyotakata1@gmail.com>
2023-03-05 17:16:47 +0900

    34.#24-1_区分のValueObject

---
+++ DDD.Domain/ValueObjects/Condition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Domain.ValueObjects
+{
+    public sealed class Condition : ValueObject<Condition>
+    {
+        public Condition(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; }
+
+        public string DisplayValue
+        {
+            get
+            {
+                if (this.Value == 1)
+                    return "晴れ";
+                else if (this.Value == 2)
+                    return "曇り";
+                else if (this.Value == 3)
+                    return "雨";
+
+                return "不明";
+
+            }
+        }
+
+        protected override bool EqualsCore(Condition other)
+        {
+            return other.Value == Value;// 自分のValueと比較
+        }
+    }
+}
