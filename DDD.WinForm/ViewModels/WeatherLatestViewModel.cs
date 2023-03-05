0ebf5e4b0cec658b3f2ec14d6f6d0415b1958aec
Kyo Takata <kyotakata1@gmail.com>
2023-03-06 07:50:22 +0900

    44.#30-4_コンボボックスへの変更

--- DDD.WinForm/ViewModels/WeatherLatestViewModel.cs
+++ DDD.WinForm/ViewModels/WeatherLatestViewModel.cs
@@ -93,7 +93,13 @@ namespace DDD.WinForm.ViewModels
         public void Search()
         {
             var entity = _ｗeather.GetLatest(Convert.ToInt32(SelectedAreaId));
-            if (entity != null)
+            if (entity == null)
+            {
+                DataDateText = string.Empty;
+                ConditionText = string.Empty;
+                TemperatureText = string.Empty;
+            }
+            else
             {
                 DataDateText = entity.DataDate.ToString();
                 ConditionText = entity.Condition.DisplayValue;
