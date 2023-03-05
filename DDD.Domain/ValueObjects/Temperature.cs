902c6f205b1309311336ebb478420ff08ec6dc06
Kyo Takata <kyotakata1@gmail.com>
2023-03-05 16:43:02 +0900

    TemperatureクラスでRoundString()の引数を修正

--- DDD.Domain/ValueObjects/Temperature.cs
+++ DDD.Domain/ValueObjects/Temperature.cs
@@ -28,7 +28,7 @@ namespace DDD.Domain.ValueObjects
         {
             get
             {
-                return Value.RoundString(2);
+                return Value.RoundString(DecimalPoint);
             }
 
         }
@@ -37,7 +37,7 @@ namespace DDD.Domain.ValueObjects
         {
             get
             {
-                return Value.RoundString(2) + UnitName;
+                return Value.RoundString(DecimalPoint) + UnitName;
             }
 
         }
@@ -46,7 +46,7 @@ namespace DDD.Domain.ValueObjects
         {
             get
             {
-                return Value.RoundString(2) +" "+ UnitName;
+                return Value.RoundString(DecimalPoint) +" "+ UnitName;
             }
 
         }
