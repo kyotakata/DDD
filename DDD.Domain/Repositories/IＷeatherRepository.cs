1d772ca25e95cdda2bbb74276e930c2f5da6dd92
Kyo Takata <kyotakata1@gmail.com>
2023-03-11 14:38:35 +0900

    48.#33-1_一覧画面実装

--- DDD.Domain/Repositories/IＷeatherRepository.cs
+++ DDD.Domain/Repositories/IＷeatherRepository.cs
@@ -1,9 +1,11 @@
 ﻿using DDD.Domain.Entities;
+using System.Collections.Generic;
 
 namespace DDD.Domain.Repositories
 {
     public interface IＷeatherRepository
     {
         WeatherEntity GetLatest(int areaId);
+        IReadOnlyList<WeatherEntity> GetData(); 
     }
 }
--- DDD.Infrastructure/SQLite/WeatherSQLite.cs
+++ DDD.Infrastructure/SQLite/WeatherSQLite.cs
@@ -8,6 +8,7 @@ namespace DDD.Infrastructure.SQLite
 {
     public class WeatherSQLite: IＷeatherRepository
     {
+
         public WeatherEntity GetLatest(int areaId)
         {
             string sql = @"
@@ -20,7 +21,7 @@ order by DataDate desc
 LIMIT 1
 ";
 
-            SQLiteHelper.QuerySingle<WeatherEntity>(sql,
+            return SQLiteHelper.QuerySingle<WeatherEntity>(sql,
                 new List<SQLiteParameter>
                 {
                     new SQLiteParameter("AreaId", areaId)
@@ -35,24 +36,12 @@ LIMIT 1
 
                 },
                 null);
-            using (var connection = new SQLiteConnection(SQLiteHelper.ConnectionString)) //SQLiteのインスタンス生成
-            using (var command = new SQLiteCommand(sql, connection))    // コマンドのインスタンス生成
-            {//usingブロックを抜けるタイミングで自動的にリソースが破棄されるようにしている
-                connection.Open();    // データベースへの接続を開く
-                command.Parameters.AddWithValue("@AreaId", areaId);
-                using (var reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        return new WeatherEntity(
-                            areaId,
-                            Convert.ToDateTime(reader["DataDate"]),
-                            Convert.ToInt32(reader["Condition"]),
-                            Convert.ToSingle(reader["Temperature"]));
-                    }
-                }
-            }
-            return null;
         }
+
+        public IReadOnlyList<WeatherEntity> GetData()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
