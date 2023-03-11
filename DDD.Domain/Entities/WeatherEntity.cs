c0490b690cfa67e8e7bbad49a65bc382060957ba
Kyo Takata <kyotakata1@gmail.com>
2023-03-11 15:23:17 +0900

    48.#33-1_一覧画面実装

--- DDD.Domain/Entities/WeatherEntity.cs
+++ DDD.Domain/Entities/WeatherEntity.cs
@@ -1,22 +1,38 @@
 ﻿using DDD.Domain.ValueObjects;
 using System;
+using System.Runtime.CompilerServices;
 
 namespace DDD.Domain.Entities
 {
     public class WeatherEntity
     {
+        public WeatherEntity(int areaId,
+            DateTime dataDate,
+            int condition,
+            float temperature)
+            :this(areaId, 
+                 string.Empty,
+                 dataDate,
+                 condition,
+                 temperature)
+        {
+        }
+
         public WeatherEntity(int areaId, 
+            string areaName, 
             DateTime dataDate, 
             int condition,
             float temperature)
         {
             AreaId= areaId;
+            AreaName= areaName;
             DataDate = dataDate;
             Condition= new Condition(condition);
             Temperature= new Temperature(temperature);
         }
 
         public int AreaId { get; }
+        public string AreaName { get; }
         public DateTime DataDate { get; }
         public Condition Condition { get; }
         public Temperature Temperature { get; }
--- DDDTest.Tests/WeatherListViewModelTest.cs
+++ DDDTest.Tests/WeatherListViewModelTest.cs
@@ -19,6 +19,7 @@ namespace DDDTest.Tests
             var entities = new List<WeatherEntity>();
             entities.Add(new WeatherEntity(
                                       1,
+                                      "東京",
                                       Convert.ToDateTime("2018/01/01 12:34:56"),
                                       2,
                                       12.3f));
@@ -32,6 +33,12 @@ namespace DDDTest.Tests
 
             var viewModel = new WeatherListViewModel(weatherMock.Object);
             viewModel.Weathers.Count.Is(2);
+            viewModel.Weathers[0].AreaId.Is("1");
+            viewModel.Weathers[0].AreaName.Is("東京");
+            viewModel.Weathers[0].DataDate.Is("2018/01/01 12:34:56");
+            viewModel.Weathers[0].Condition.Is("曇り");
+            viewModel.Weathers[0].Temperature.Is("12.30 ℃");
+
         }
     }
 }
