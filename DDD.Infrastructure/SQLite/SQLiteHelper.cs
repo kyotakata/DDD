ac4b56c676fc256fb48dd8ef0e2a776686d15d7a
Kyo Takata <kyotakata1@gmail.com>
2023-03-12 15:18:38 +0900

    61.#37_Save画面のSQLite実装

--- DDD.Infrastructure/SQLite/SQLiteHelper.cs
+++ DDD.Infrastructure/SQLite/SQLiteHelper.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,7 @@ namespace DDD.Infrastructure.SQLite
     internal class SQLiteHelper
     {
         internal const string ConnectionString = @"Data Source=C:\Users\kyota\source\repos\DDD\DDD.db;Version=3;";
-        
+
         internal static IReadOnlyList<T> Query<T>(
                     string sql,
                     Func<SQLiteDataReader, T> createEntity
@@ -20,11 +21,11 @@ namespace DDD.Infrastructure.SQLite
             return Query<T>(sql, null, createEntity);
         }
 
-            internal static IReadOnlyList<T> Query<T>(
-                            string sql,
-                            SQLiteParameter[] parameters,
-                            Func<SQLiteDataReader, T> createEntity
-                            )
+        internal static IReadOnlyList<T> Query<T>(
+                        string sql,
+                        SQLiteParameter[] parameters,
+                        Func<SQLiteDataReader, T> createEntity
+                        )
         {
             var result = new List<T>();
             using (var connection = new SQLiteConnection(SQLiteHelper.ConnectionString))
@@ -32,7 +33,7 @@ namespace DDD.Infrastructure.SQLite
             {
                 connection.Open();
 
-                if(parameters != null)
+                if (parameters != null)
                 {
                     command.Parameters.AddRange(parameters);
                 }
@@ -72,7 +73,7 @@ namespace DDD.Infrastructure.SQLite
                 {
                     command.Parameters.AddRange(parameters);
                 }
-                
+
                 using (var reader = command.ExecuteReader())
                 {
                     while (reader.Read())
@@ -83,6 +84,48 @@ namespace DDD.Infrastructure.SQLite
             }
 
             return nullEntity;
+        }
+
+        internal static void Execute(
+            string insert,
+            string update,
+            SQLiteParameter[] parameters)
+        {
+            using (var connection = new SQLiteConnection(SQLiteHelper.ConnectionString))
+            using (var command = new SQLiteCommand(update, connection))// updateから行う
+            {
+                connection.Open();
+
+                if (parameters != null)
+                {
+                    command.Parameters.AddRange(parameters);
+                }
+
+                if (command.ExecuteNonQuery() < 1)// ExecuteNonQuery()は件数が返ってくる。update実行したけど対象がなかったら(0件なら)
+                {
+                    command.CommandText = insert;// insertに変更して、以下を再び実行
+                    command.ExecuteNonQuery(); // ExecuteNonQuery()実行
+                }
+
+            }
+
+        }
+
+        internal static void Execute(
+            string sql,
+            SQLiteParameter[] parameters)
+        {
+            using (var connection = new SQLiteConnection(SQLiteHelper.ConnectionString))
+            using (var command = new SQLiteCommand(sql, connection))// updateから行う
+            {
+                connection.Open();
+
+                if (parameters != null)
+                {
+                    command.Parameters.AddRange(parameters);
+                }
+            }
+
         }
 
     }
