0f74dd07376bf8c8f6b11d4f59d92cb82bc846b0
Kyo Takata <kyotakata1@gmail.com>
2023-03-12 10:20:35 +0900

    53.#34-2_Save画面の実装

---
+++ DDD.WinForm/ViewModels/WeatherSaveViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.WinForm.ViewModels
+{
+    public class WeatherSaveViewModel : ViewModelBase
+    {
+        public WeatherSaveViewModel()
+        {
+            DataDateValue = DateTime.Now;
+        }
+
+        // 基本的にViewModelのプロパティの型はコントロールのバインディングする型と合わせる
+        public object SelectedAreaId { get; set; }    // ConboBoxのValueプロパティをデータバインドするのでobject型
+        public DateTime DataDateValue { get; set; }
+        public object SelectedCondition { get; set; }    // ConboBoxのValueプロパティをデータバインドするのでobject型
+        public string TemperatureText { get; set; }    // TextBoxのTextプロパティをデータバインドするのでstring型
+    }
+}
