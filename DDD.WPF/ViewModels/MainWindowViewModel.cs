bfb9739c7f7b4ecf39a3de8831e8a7c18e9f0fc8
Kyo Takata <kyotakata1@gmail.com>
2023-03-15 07:29:31 +0900

    32.S2_#02_メイン画面の作成

--- DDD.WPF/ViewModels/MainWindowViewModel.cs
+++ DDD.WPF/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ namespace DDD.WPF.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "DDD";
         public string Title
         {
             get { return _title; }
