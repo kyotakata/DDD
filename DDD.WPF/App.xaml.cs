d519b410acaf94803dea3eab145a2376a91b3d94
Kyo Takata <kyotakata1@gmail.com>
2023-03-15 21:48:07 +0900

    36.S2_#03-4_メイン画面から直近値画面の画面遷移

--- DDD.WPF/App.xaml.cs
+++ DDD.WPF/App.xaml.cs
@@ -16,7 +16,7 @@ namespace DDD.WPF
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
-
+            containerRegistry.RegisterForNavigation<WeatherLatestView>();
         }
     }
 }
--- DDD.WPF/ViewModels/MainWindowViewModel.cs
+++ DDD.WPF/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,13 @@
-﻿using Prism.Mvvm;
+﻿using DDD.WPF.Views;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
 
 namespace DDD.WPF.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
+        private IRegionManager _regionManager;
         private string _title = "DDD";
         public string Title
         {
@@ -11,9 +15,18 @@ namespace DDD.WPF.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
-        public MainWindowViewModel()
+        public MainWindowViewModel(IRegionManager regionManager)
         {
+            _regionManager = regionManager;
+            WeatherLatestButton = new DelegateCommand(
+                WeatherLatestButtonExecute);
+        }
 
+        public DelegateCommand WeatherLatestButton { get; }
+
+        private void WeatherLatestButtonExecute()
+        {
+            _regionManager.RequestNavigate("ContentRegion", nameof(WeatherLatestView));
         }
     }
 }
