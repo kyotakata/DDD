4787eb52f8d50e4ee7179f38361dece5212f6343
Kyo Takata <kyotakata1@gmail.com>
2023-03-16 07:06:37 +0900

    40.S2_#05_例外通知を全体で受けてメッセージボックスを表示する

--- DDD.WPF/App.xaml.cs
+++ DDD.WPF/App.xaml.cs
@@ -9,6 +9,29 @@ namespace DDD.WPF
     /// </summary>
     public partial class App
     {
+        public App()
+        {
+            this.DispatcherUnhandledException
+                += App_DispatcherUnhandledException;   // 処理されない例外という意味
+        }
+
+        /// <summary>
+        /// アプリケーションの例外がキャッチされていない場合にここまで落ちてくる
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void App_DispatcherUnhandledException(
+            object sender, 
+            System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(
+                e.Exception.Message,
+                "メッセージ",
+                MessageBoxButton.OK, 
+                MessageBoxImage.Information);
+            e.Handled = true;// これを書いておけばアプリケーションは落ちずに正しく処理されたことを示す
+        }
+
         protected override Window CreateShell()
         {
             return Container.Resolve<MainWindow>();
